3899ad222966b860a1d6f6ebd63de3852d594d16
meng tao <mengtao.hnu@gmail.com>
2014-12-02 21:53:43 +0800

    控制结构

---
+++ PythonBasis/cn/hnu/flow/If.py
@@ -0,0 +1,19 @@
+'''
+Created on 2014年12月2日
+
+@author: mengtao
+'''
+
+# 控制结构 if
+
+number = 23;
+guess = int(input('please enter an number!'));
+
+if guess == number:
+    print('恭喜你答对了');
+elif guess < number:
+    print('猜小了');
+else:
+    print('猜大了');
+
+
---
+++ PythonBasis/cn/hnu/flow/__init__.py
@@ -0,0 +1,5 @@
+'''
+Created on 2014年11月30日
+
+@author: mengtao
+'''
