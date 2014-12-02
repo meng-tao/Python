dd50ea50860b1c302ba3050c81c54ff91651ae06
meng tao <mengtao.hnu@gmail.com>
2014-12-02 22:10:11 +0800

    修改

---
+++ PythonBasis/cn/hnu/flow/Break.py
@@ -0,0 +1,12 @@
+'''
+Created on 2014年12月2日
+
+@author: mengtao
+'''
+# 控制结构，break语句
+
+while True :
+    s = input('pleases enter an string!\n');
+    if s == 'quit' :
+        break;
+    print('字符串的长度为：', len(s));
