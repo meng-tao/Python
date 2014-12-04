ff6b20caeb8c6ab705e612af074ab6dad4c293cf
meng tao <mengtao.hnu@gmail.com>
2014-12-04 22:09:45 +0800

    函数参数

---
+++ PythonBasis/cn/hnu/function/Function_Param.py
@@ -0,0 +1,16 @@
+'''
+Created on 2014年12月4日
+
+@author: mengtao
+'''
+# 参数
+
+def fun_param(a, b):
+    if a > b:
+        print('a大于b');
+    elif a < b:
+        print('a小于b');
+    else :
+        print('相等');
+        
+fun_param(2, 2);
