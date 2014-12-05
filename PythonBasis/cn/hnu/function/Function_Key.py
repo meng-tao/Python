1e7ecbddaeeb673ae9c073886419d84fe621d94f
meng tao <mengtao.hnu@gmail.com>
2014-12-05 22:05:06 +0800

    关键值

---
+++ PythonBasis/cn/hnu/function/Function_Key.py
@@ -0,0 +1,13 @@
+'''
+Created on 2014年12月5日
+
+@author: mengtao
+'''
+# 关键参数
+
+def fun_key(a, b = 3, c = 5):
+    print('a的值为：', a, 'b的值为：', b, 'c的值为：', c);
+    
+fun_key(3, 7, 9);
+fun_key(1, 4);
+fun_key(10, c = 50);
